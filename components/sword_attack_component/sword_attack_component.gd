28183ae707b6614ad29cd9e99731f1ebb80f1c1b
saltchicken <john.eicher89@gmail.com>
2024-06-30 10:34:54 -0700

    Add tree object

--- components/sword_attack_component/sword_attack_component.gd
+++ components/sword_attack_component/sword_attack_component.gd
@@ -6,8 +6,8 @@ func _ready():
 	self.area_entered.connect(_on_hitbox_area_entered)
 
 func _on_hitbox_area_entered(area):
+	print(area)
 	if area is HitboxComponent and area.owner != owner:
-		print('Hitbox hit')
 		var hitbox : HitboxComponent = area
 		
 		var attack = Attack.new()
