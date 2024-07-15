78604166b98ad6cbd1c554ffaaf6de29bc2bcd9f
saltchicken <john.eicher89@gmail.com>
2024-07-15 10:33:52 -0700

    Reorganize objects in level one

--- scene_manager/main_menu/main_menu.gd
+++ scene_manager/main_menu/main_menu.gd
@@ -9,23 +9,14 @@ func _ready():
 		#button.pressed.connect(button_pressed.bind(button))
 	for button in buttons:
 		button.button_up.connect(_on_button_button_up.bind(button))
-		
-#func custom_change_scene(scene):
-	#get_tree().root.add_child(scene_instance)
-	#get_tree().current_scene = scene_instance
-	#scene_instance.add_child(player_instance)
-	#scene_instance.player = player_instance
-	
+			
 func _on_button_button_up(button):
 	match button.text:
 		"New Game":
 			SceneManager.swap_scenes("res://menus/profile_creation_menu/profile_creation.tscn",get_tree().root,self,"fade_to_black")
-			
+		
 		"Continue":
 			SceneManager.swap_scenes("res://menus/profile_selection_menu/profile_selection_menu.tscn",get_tree().root,self,"fade_to_black")
-
-			
-			#queue_free()
 			
 		"Exit To Deskop":
 			get_tree().quit()
