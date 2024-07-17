8230ab35065818cb68868d09c00f7d4847e826ba
saltchicken <john.eicher89@gmail.com>
2024-07-17 14:10:54 -0700

    Add level_parent for inheritence for level creation

--- scene_manager/gameplay/scripts/level.gd
+++ scene_manager/gameplay/scripts/level.gd
@@ -111,6 +111,37 @@ func disconnect_portals() -> void:
 			portal.player_entered.disconnect(on_player_entered_portal)
 			#print_debug("Disconnected %s" % portal.name)
 			
+func save():
+	#var save_nodes = get_tree().get_nodes_in_group("LevelPersist")
+	#for save_node in save_nodes:
+		#print(save_node)
+		
+	var trees = save_trees()
+	var chests = save_chests()
+		
+	var save_dict = {
+		"filename" : get_scene_file_path(),
+		"node_path" : get_path(),
+		"name" : name,
+		"trees" : trees,
+		"chests" : chests,
+		#"bonfires" : bonfires,
+		#"enemies" : enemies
+	}
+	return save_dict
+
+func save_trees():
+	var save_dict = {}
+	for tree in $Trees.get_children():
+		save_dict[tree.name] = tree.save()
+	return save_dict
+	
+func save_chests():
+	var save_dict = {}
+	for chest in $Chests.get_children():
+		save_dict[chest.name] = chest.save()
+	return save_dict
+			
 #func register_bonfires():
 	#for bonfire in get_tree().get_nodes_in_group("Bonfires"):
 		#bonfire.area_2d.body_entered.connect(bonfire_body_entered.bind(bonfire))
