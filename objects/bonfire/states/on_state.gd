071f7c4daf9241ba8c92f4f474a77bc3e5cd0a59
saltchicken <john.eicher89@gmail.com>
2024-07-18 18:36:12 -0700

    Add bonfire on sound

--- objects/bonfire/states/on_state.gd
+++ objects/bonfire/states/on_state.gd
@@ -3,7 +3,10 @@ extends State
 @onready var character_body = self.get_owner()
 @onready var animated_sprite = $"../../AnimatedSprite2D"
 
+@export var sfx : AudioStreamPlayer2D
+
 func Enter():
+	sfx.play()
 	animated_sprite.play("on")
 	
 func Exit():
