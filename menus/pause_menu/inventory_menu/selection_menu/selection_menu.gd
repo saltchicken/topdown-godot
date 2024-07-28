50aa9ac1b6f2fb8e856bad87af240662c9981b89
saltchicken <john.eicher89@gmail.com>
2024-07-28 11:36:57 -0700

    Implement matching for selection_menu buttons. +cleanup

--- menus/pause_menu/inventory_menu/selection_menu/selection_menu.gd
+++ menus/pause_menu/inventory_menu/selection_menu/selection_menu.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
-#@onready var equip_button = get_node('PanelContainer/HBoxContainer/EquipButton')
-#@onready var drop_button = get_node('PanelContainer/HBoxContainer/DropButton')
-
 @onready var buttons = get_node('PanelContainer/HBoxContainer').get_children()
-
 @onready var selected_button: int = 0: set = _set_selected_button
 
 func _set_selected_button(new_value):
@@ -16,19 +12,17 @@ func _set_selected_button(new_value):
 		selected_button = new_value
 	buttons[selected_button].grab_focus()
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for button in buttons:
 		button.pressed.connect(selection_menu_button_pressed.bind(button))
-	
-	#equip_button.pressed.connect(on_equip_pressed)
-	#drop_button.pressed.connect(on_drop_pressed)
-	
-#func on_equip_pressed():
-	#print_debug("Need to implement equip pressed")
-	#
-#func on_drop_pressed():
-	#print_debug("Need to implement drop pressed")
 
 func selection_menu_button_pressed(button):
-	print_debug(button, " not implemented")
+	match button.name:
+		"EquipButton":
+			print_debug(button, " matched but not implemented")
+		"DropButton":
+			print_debug(button, " matched but not implemented")
+		"MoveButton":
+			print_debug(button, " matched but not implemented")
+		_:
+			print_debug(button, " not implemented")
