51e7e3a27a32f4cce86d55847e570b750202b65e
saltchicken <john.eicher89@gmail.com>
2024-08-03 11:41:36 -0700

    Add toolbelt to HUD

---
+++ menus/hud/tool_belt.gd
@@ -0,0 +1,8 @@
+extends HBoxContainer
+
+
+func _gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("I've been clicked D:")
+	pass
