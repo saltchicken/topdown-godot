001512cf85601f3c1eb54902157ef4661a992380
saltchicken <john.eicher89@gmail.com>
2024-08-11 21:11:42 -0700

    Implement _tile_available for placing tile_item

--- items/components/place/place.gd
+++ items/components/place/place.gd
@@ -15,7 +15,7 @@ func place(player):
 	tile_map_coords += Vector2i(player.direction)
 	if _tile_available(tile_map_coords): # TODO: This needs to check if the coord is already taken
 		#tile_select_layer.set_cell(current_highlighted_tile_coords, -1, Vector2i(-1,-1), -1)
-		print("planting seed   ", str(tile_map_coords))
+		#print("planting seed   ", str(tile_map_coords))
 		player_object_layer.set_cell(tile_map_coords, 0, Vector2i(0,0), 3)
 		return true
 		#current_highlighted_tile_coords = tile_map_coords
@@ -26,4 +26,15 @@ func place(player):
 	# TODO: If this works then need to consume like Health Potion
 
 func _tile_available(tile_map_coords):
-	return true
+	var current_level = get_node('/root/Gameplay').current_level
+	var player_object_layer = current_level.get_node_or_null('PlayerObjectLayer')
+	#print(player_object_layer.get_cell_tile_data(tile_map_coords))
+	#print(player_object_layer.get_used_cells())
+	#print(player_object_layer.get_cell_source_id(tile_map_coords))
+	if player_object_layer.get_cell_source_id(tile_map_coords) == -1:
+		return true
+	elif player_object_layer.get_cell_source_id(tile_map_coords) == 0:
+		return false
+	else:
+		push_error("This hasn't been implemented yet, just going to say this cell is available")
+		return true
