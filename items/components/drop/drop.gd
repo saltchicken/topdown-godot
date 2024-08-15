3a50ff563f49fd7c50dcbc193179f2ea2d6f805b
saltchicken <john.eicher89@gmail.com>
2024-08-14 21:00:14 -0700

    Remove collision layers and add bounce to item drop

--- items/components/drop/drop.gd
+++ items/components/drop/drop.gd
@@ -12,20 +12,22 @@ func _ready() -> void:
 func drop_items():
 	for drop in drops:
 		_drop(drop)
+		#await get_tree().create_timer(0.01).timeout
 	
 func _drop(item):
 	remove_child(item)
 	var dropper = dropper_node.instantiate()
-	dropper.global_position = global_position + Vector2(randf() * 16, randf() * 16)
+	dropper.global_position = global_position #  + Vector2(randf() * 4, randf() * 4)
 	item.position = Vector2(0.0,0.0)
 	dropper.item_to_drop = item
-	dropper.add_child(item)
+	dropper.get_node("Item").add_child(item)
 	if item is InventoryItem:
 		item.size = Vector2(32.0, 32.0) # TODO: Better handling of size forcing
+		item.position += Vector2(-16.0, -16.0)
 	#_enable_item(item) 
 	item.visible = true
 	get_tree().current_scene.get_node("LevelHolder").get_children()[0].add_child(dropper) # TODO: Replace this with a call to Global
-	dropper.velocity = Vector2(0.0, -10.0).rotated(randf_range(-0.5, 0.5)) * randf_range(0.9, 1.5)
+	dropper.velocity = Vector2(0.0, 200.0).rotated(randf_range(-1.5, 1.5))  * randf_range(0.9, 1.5)
 	#dropper.velocity = owner.velocity.rotated(randf_range(-1.5, 1.5)) * randf_range(0.9, 1.5)
 	
 	
--- items/components/drop/dropper/dropper.gd
+++ items/components/drop/dropper/dropper.gd
@@ -13,14 +13,16 @@ func _physics_process(delta: float) -> void:
 		push_error("Dropper does not have an item")
 		return
 	var collision = move_and_collide(velocity * delta)
-	if collision:
+	if collision: # TODO: Set proper collision layer for this to work
 		velocity = velocity.bounce(collision.get_normal())
-	velocity -= velocity * delta * 2
+	velocity -= velocity * delta * 6
 
 func _on_timeout():
 	print(item_to_drop)
-	remove_child(item_to_drop)
+	get_node("Item").remove_child(item_to_drop)
 	item_to_drop.global_position = global_position
+	if item_to_drop is InventoryItem:
+		item_to_drop.position += Vector2(-16.0, -16.0)
 	item_to_drop.process_mode = Node.PROCESS_MODE_INHERIT
 	get_tree().current_scene.get_node("LevelHolder").get_children()[0].add_child(item_to_drop)
 	queue_free()
